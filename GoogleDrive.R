40940217d1761e213aafeb12bb5c6bf5d7d8c06a
gtonychuo <tonychuo@g-mail.nsysu.edu.tw>
2019-02-25 00:05:28 +0800

    GoogleDrive.R improved

--- GoogleDrive.R
+++ GoogleDrive.R
@@ -1,3 +1,11 @@
 library("googledrive")
 drive_auth()
-drive_ls(path="2019 程式、機率與商務/2019RPB/unit01/DC認證：R語言導論")
+
+### Unit01 作業
+RPB = "2019 程式、機率與商務/2019RPB/unit01/"
+d1 = drive_ls(paste0(RPB, "DC認證：R語言導論"))
+name1 = gsub("(_|＿).*", "", d1$name)
+d2 = drive_ls(paste0(RPB, "DC認證：資料框整理技巧"))
+name2 = gsub("(_|＿).*", "", d2$name)
+setdiff(name1, name2)
+
